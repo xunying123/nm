9eec9e5e9d519184e35a21ace538a0e15ec9695b
xunying123 <119675097+xunying123@users.noreply.github.com>
2024-01-11 07:45:32 +0800

    Add files via upload

---
+++ riscv/src/func/IsBranch.v
@@ -0,0 +1,12 @@
+
+module IsBranch (
+	input wire [`INST_TYPE_WIDTH] type,
+	output reg is_Branch
+);
+always @(*) begin
+	if(type==`BEQ||type==`BNE||type==`BLT||type==`BGE||type==`BLTU||type==`BGEU||type==`JAL||type==`JALR)
+		is_Branch=1;
+	else is_Branch=0;
+end
+
+endmodule
