80c9914cb94417166f51bb922c582b4c5afcc57e
xunying123 <119675097+xunying123@users.noreply.github.com>
2024-01-11 07:33:18 +0800

    Add files via upload

---
+++ riscv/src/func/IsLoad.v
@@ -0,0 +1,13 @@
+
+
+module IsLoad (
+	input wire [`INST_TYPE_WIDTH] type,
+	output reg is_Load
+);
+always @(*) begin
+	if(type==`LB||type==`LH||type==`LW||type==`LBU||type==`LHU)
+		is_Load=1;
+	else is_Load=0;
+end
+
+endmodule
